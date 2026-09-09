badc68da19b23996ac70a33a2290bde51a56fa53
Isha Vats <ivats.nic@gmail.com>
2026-09-09 16:25:49 +0530

    new chnages been made

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -3,17 +3,17 @@ import { Link } from "react-router-dom";
 function CategoryCard(props) {
     return (
         <Link to={props.path}>
-            <div className={`relative h-40 rounded-[28px] ${props.color} `}>
+            <div className={`relative h-35 w-77 rounded-[28px] ${props.color} `}>
                 <div className={`absolute -top-7 left-1/2 -translate-x-1/2 rounded-full border border-white px-7 py-2 ${props.color}`}>
                     <h2 className="whitespace-nowrap text-xl font-bold">
                         {props.title}
                     </h2>
                 </div>
-                <p className="flex h-full items-center px-8 font-bold sm:text-2xl">
+                <img src={props.image}
+                    className="absolute z-10 h-20 left-[65%] -translate-x-[10%] top-6 "></img>
+                <p className="flex h-full items-center px-8 font-semibold md:text-xl sm:text-2xl w-55">
                     {props.description}
                 </p>
-                <img src={props.image} 
-                className="absolute z-10 h-28 left-[65%] -translate-x-[10%] top-6 "></img>
             </div>
         </Link>
     );
