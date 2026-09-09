bf6f673d57cb005b76f1acbd4122301d6a58a435
Isha Vats <ivats.nic@gmail.com>
2026-09-09 14:23:31 +0530

    new chnages been made

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -1,6 +1,6 @@
 function CategoryCard(props) {
     return (
-        <div className={`relative h-40 rounded-[28px] border-2 border-white ${props.color}`}>
+        <div className={`relative h-40 rounded-[28px] ${props.color}`}>
             <div className={`absolute -top-7 left-1/2 -translate-x-1/2 rounded-full border border-white px-7 py-2 ${props.color}`}>
                 <h2 className="whitespace-nowrap text-xl font-bold">
                     {props.title}
