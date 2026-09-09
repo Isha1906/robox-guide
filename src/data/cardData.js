e77ec2c827fa83607354c1b88d38f2b4263317b5
Isha Vats <ivats.nic@gmail.com>
2026-09-09 14:24:22 +0530

    new chnages been made

--- src/data/cardData.js
+++ src/data/cardData.js
@@ -2,22 +2,22 @@ const records=[
     {
         title:"RBX TOOLS",
         description:"Guide Free Skins & Emotes for RBX",
-        color:"bg-pink-200"
+        color:"bg-pink-300"
     },
     {
         title:"CHARACTERS",
         description:"Guide Free Characters & Avatars for RBX",
-        color:"bg-red-200"
+        color:"bg-red-300"
     },
     {
         title:"FREE RBX",
         description:"Guide for Daily FREE RBX",
-        color:"bg-blue-200"
+        color:"bg-blue-300"
     },
     {
         title:"MAPS",
         description:"Guide Free Maps & Levels",
-        color:"bg-purple-200"
+        color:"bg-purple-300"
     }
 ]
 export default records
